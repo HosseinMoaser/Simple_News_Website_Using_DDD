08fe5dab4db216b23b22e1377f12bba918bb273c
Hossein Moaser <moaser.hossein@gmail.com>
2023-09-15 01:27:20 +0330

    Initial Commit

---
+++ RoshanTarAzAftab.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RoshanTarAzAftab.App;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RoshanTarAzAftab.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RoshanTarAzAftab.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
