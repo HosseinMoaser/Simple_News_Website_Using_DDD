50527156ae29b2cde07a803da1a8eade66003343
Hossein Moaser <moaser.hossein@gmail.com>
2023-09-24 23:13:35 +0330

    Create Application Layer

---
+++ RoshanTarAzAftab.Application/Services/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using RoshanTarAzAftab.Domain.ValueObjects.Message;
+
+namespace RoshanTarAzAftab.Application.Services;
+
+public interface IEmailService
+{
+    Task SendEmailAsync(Email email,string message);
+}
